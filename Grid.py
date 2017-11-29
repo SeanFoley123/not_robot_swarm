499c830b45d7522167426ae3c8d2ef0e6973cce6
Sean Foley <sean.foley@newgridinc.com>
2017-11-28 19:53:52 -0500

    Intermediate

--- Grid.py
+++ Grid.py
@@ -2,21 +2,21 @@ from Vertex import *
 
 class Grid(object):
 
+    def __init__(self):
+        self.list_of_vertices = []
+
+
+class CompleteGraph(Grid):
+
     def __init__(self, num):
-        self.list_of_vertices = []
-        self.generate_complete_graph(num)
+        super().__init__()
 
-    def generate_complete_graph(self, num_of_vertices):
+        self.list_of_vertices = [Vertex(name='v'+str(i)) for i in range(num)]
 
-        self.list_of_vertices = []
-        for i in range(num_of_vertices):
+        for i in range(num):
             connections = []
-            for j in range(num_of_vertices):
+            for j in range(num):
                 if not j == i:
                     connections.append('v'+str(j))
 
-
-            v = Vertex(name='v'+str(i), names_of_connections = connections)
             self.list_of_vertices.append(v)
-
-
--- visualization.py
+++ visualization.py
@@ -2,6 +2,8 @@ import sys, os, random
 import pygame
 from pygame.locals import *
 from math import sqrt
+from Vertex import Vertex
+from Grid import CompleteGraph
 
 def main():
 	pygame.init()
@@ -18,14 +20,16 @@ def main():
 	background = pygame.Surface((sizex, sizey))
 	background.fill(background_color)
 
-	vertex_set = {1: [2, 3, 4], 2: [1, 4], 3: [1], 4: [1, 2], 5: [1, 2], 6: [5, 6], 8:[], 9:[], 10:[]}
+	complete = CompleteGraph(10)
+	# vertex_set = {1: [2, 3, 4], 2: [1, 4], 3: [1], 4: [1, 2], 5: [1, 2], 6: [5, 6], 8:[], 9:[], 10:[]}
+	
 	vertex_coords = dict()
- 
 	margin = 100
-	spacing = int((sizex - 2*margin)/sqrt(len(vertex_set.keys())))
+	spacing = int((sizex - 2*margin)/sqrt(len(complete.list_of_vertices)))
 	x = 0
 	y = 0
-	for vertex in vertex_set.keys():
+
+	for vertex in complete.list_of_vertices:
 		pygame.draw.circle(background, vertex_color, (x + margin, y + margin), 5)
 		vertex_coords[vertex] = (x + margin, y + margin)
 		x += spacing
@@ -33,8 +37,8 @@ def main():
 			x = 0
 			y += spacing
 		
-	for start_vertex in vertex_set.keys():
-		for end_vertex in vertex_set[start_vertex]:
+	for start_vertex in complete.list_of_vertices:
+		for end_vertex in vertex.names_of_connections:
 			pygame.draw.line(background, vertex_color, vertex_coords[start_vertex], vertex_coords[end_vertex])
 
 	while True:
